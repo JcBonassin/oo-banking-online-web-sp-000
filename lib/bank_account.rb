e8234936ae89e77cb20ec459500c28df841da4ef
JC <bonassin@gmail.com>
2021-01-11 16:51:18 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
   attr_reader :name
-  attr_accessor :balance
+  attr_accessor :balance, :status
 
   def initilize(name)
     @name = name
     @balance = 100
+    @status = "open"
   end
 
 end
